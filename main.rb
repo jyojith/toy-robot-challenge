3316b406ec768782af31da5807855105becc00a1
Jyojith George Thaliath <jyojith@bigbinary.com>
2022-06-02 02:12:24 +0200

    minute bugfixes and fixing issue with rotation of direction and third use case

--- main.rb
+++ main.rb
@@ -1,13 +1,26 @@
 require_relative "robot.rb"
 
-robot = Robot.new()
+@robot = Robot.new()
 
-#print robot.report()
- while(true)
-   command = gets.chomp()
-   robot.read_command(command)
-   if command == "REPORT"
-     puts robot.report()
-     break
-   end
+# while(true)
+#   command = gets.chomp()
+#   robot.read_command(command)
+#   if command == "REPORT"
+#     puts robot.report()
+#     break
+#   end
+# end
+@commands_4 = ["PLACE 5,5,NORTH","REPORT"]
+
+def execute(commands)
+  for command in commands do
+    @robot.read_command(command)
+    if command == "REPORT"
+      output = @robot.report()
+      break
+    end
+  end
+  return output
 end
+
+puts execute(@commands_4)
--- robot.rb
+++ robot.rb
@@ -2,9 +2,8 @@ class Robot
   attr_accessor :x ,:y, :direction, :args
 
   def initialize()
-    @x = 0
-    @y = 0
-    @direction = "NORTH"
+    @x = -1
+    @y = -1
   end
 
   def read_command(string)
@@ -31,8 +30,6 @@ class Robot
     if self.check_on_board(x,y)
       @x,@y = x,y
       @direction = args[2]
-    else
-      return
     end
   end
 
@@ -46,19 +43,15 @@ class Robot
       x = x + 1
     elsif @direction == "WEST"
       x = x - 1
-    else
-      self.invalid_direction()
     end
-    if check_on_board(x,y)
+    if self.check_on_board(x,y)
       @x,@y = x,y
-    else
-      return
     end
   end
 
   def report()
     if self.check_on_board(x,y)
-      text = "Output: #{@x}, #{@y}, #{@direction}"
+      text = "Output: #{@x},#{@y},#{@direction}"
     else
       text = "Not on board"
     end
@@ -66,46 +59,37 @@ class Robot
   end
 
   def left()
-    if self.check_on_board(x,y)
-      @x,@y = x,y
-    else
-      return
-    end
     if @direction == "NORTH"
       @direction = "WEST"
-    elsif @direction == "EAST"
-      @direction == "NORTH"
-    elsif @direction == "SOUTH"
-      @direction == "WEST"
     elsif @direction == "WEST"
-      @direction == "SOUTH"
+      @direction = "SOUTH"
+    elsif @direction == "SOUTH"
+      @direction = "EAST"
+    elsif @direction == "EAST"
+      @direction = "NORTH"
     end
   end
 
   def right()
-    if self.check_on_board(x,y)
-      @x,@y = x,y
-    else
-      return
-    end
     if @direction == "NORTH"
       @direction = "EAST"
     elsif @direction == "EAST"
-      @direction == "SOUTH"
+      @direction = "SOUTH"
     elsif @direction == "SOUTH"
-      @direction == "WEST"
+      @direction = "WEST"
     elsif @direction == "WEST"
-      @direction == "NORTH"
+      @direction = "NORTH"
     end
   end
 
   def check_on_board(x,y)
-    return (x >=0 && x<=5)&&( y>=0 && y<=5)
+    return (x >=0 && x<=4)&&( y>=0 && y<=4)
   end
 
   def invalid_direction()
     return "Invalid Direction"
   end
+
   def invalid_command()
     return "Invalid Command"
   end
